65ea6494933ee265be83ad5f5916c0c257a3d5bc
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-10-25 16:42:41 +0100

    Restructured tests

--- test/0-2/part-time-15.js
+++
@@ -1,9 +0,0 @@
-'use-strict';
-
-const {expect} = require('chai');
-
-describe('15 hours free childcare', () => {
-  it('should return 255 per week', () => {
-    expect(true).to.eql(true);
-  });
-});
--- test/test.js
+++ test/test.js
@@ -7,21 +7,3 @@ describe('--unit tests', () => {
   require('./unit-tests/getCostPerHalfDay');
   require('./unit-tests/getCostPerWeek');
 });
-
-// // Integration tests
-// describe('--integration tests', () => {
-// });
-
-// // 0-2
-// describe('--0-2 year olds', () => {
-//   require('./0-2/full-time');
-//   require('./0-2/part-time-15');
-//   require('./0-2/part-time-30');
-// });
-
-// // 2+
-// describe('--0-2 year olds', () => {
-//   require('./2+/full-time');
-//   require('./2+/part-time-15');
-//   require('./2+/part-time-30');
-// });
