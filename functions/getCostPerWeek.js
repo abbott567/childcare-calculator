69ffb03e475d6baff86699daee5633048b25f7e8
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-10-26 14:13:39 +0100

    Added tests for costPerWeek

--- functions/getCostPerWeek.js
+++ functions/getCostPerWeek.js
@@ -3,7 +3,7 @@
 const {getCostPerDay} = require('../functions/getCostPerDay');
 const {getCostPerHalfDay} = require('../functions/getCostPerHalfDay');
 
-function getCostPerWeek(daysArray, age) {
+function getCostPerWeek(daysArray, age, freeHours) {
   const costPerDay = getCostPerDay(age);
   const costPerHalfDay = getCostPerHalfDay(age);
   let costPerWeek = 0;
@@ -16,6 +16,19 @@ function getCostPerWeek(daysArray, age) {
       costPerWeek += costPerHalfDay;
     }
   }
+
+  if (freeHours === '15' || freeHours === 15) {
+    costPerWeek -= (costPerDay + costPerHalfDay);
+  }
+
+  if (freeHours === '30' || freeHours === 30) {
+    costPerWeek -= (costPerDay * 3);
+  }
+
+  if (costPerWeek < 0) {
+    costPerWeek = 0;
+  }
+
   return costPerWeek;
 }
 
---
+++ test/unit-tests/getCostsPerWeek/30-hours-free/toddler.js
@@ -0,0 +1,98 @@
+'use-strict';
+
+const {expect} = require('chai');
+const {getCostPerDay} = require('../../../../functions/getCostPerDay');
+const {getCostPerHalfDay} = require('../../../../functions/getCostPerHalfDay');
+const {getCostPerWeek} = require('../../../../functions/getCostPerWeek');
+
+const age = 2;
+const costPerDay = getCostPerDay(age);
+const costPerHalfDay = getCostPerHalfDay(age);
+const freeChildCare = costPerDay * 3;
+
+describe('full time', () => {
+  it(`should return ${(costPerDay * 5) - freeChildCare} if 5 full days and 0 half days`, () => {
+    const daysArray = ['full', 'full', 'full', 'full', 'full'];
+    const costPerWeek = getCostPerWeek(daysArray, age, 30);
+    expect(costPerWeek).equal((costPerDay * 5) - freeChildCare);
+  });
+});
+
+describe('part time', () => {
+  it('should return 0 if 0 full days and 1 half day', () => {
+    const daysArray = ['half', 'none', 'none', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 0 full days and 2 half days', () => {
+    const daysArray = ['half', 'half', 'none', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 0 full days and 3 half days', () => {
+    const daysArray = ['half', 'half', 'half', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 0 full days and 4 half days', () => {
+    const daysArray = ['half', 'half', 'half', 'half', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 0 full days and 5 half days', () => {
+    const daysArray = ['half', 'half', 'half', 'half', 'half'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 1 full day and 0 half days', () => {
+    const daysArray = ['full', 'none', 'none', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 1 full day and 1 half day', () => {
+    const daysArray = ['full', 'half', 'none', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 1 full day and 2 half days', () => {
+    const daysArray = ['full', 'half', 'half', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 1 full day and 3 half days', () => {
+    const daysArray = ['full', 'half', 'half', 'half', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it(`should return ${costPerDay + (costPerHalfDay * 4) - freeChildCare} if 1 full day and 4 half days`, () => {
+    const daysArray = ['full', 'half', 'half', 'half', 'half'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(costPerDay + (costPerHalfDay * 4) - freeChildCare);
+  });
+  it('should return 0 if 2 full days and 0 half days', () => {
+    const daysArray = ['full', 'full', 'none', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it('should return 0 if 2 full days and 1 half days', () => {
+    const daysArray = ['full', 'full', 'half', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal(0);
+  });
+  it(`should return ${(costPerDay * 2) + (costPerHalfDay * 2) - freeChildCare} if 2 full days and 2 half days`, () => {
+    const daysArray = ['full', 'full', 'half', 'half', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 2) + (costPerHalfDay * 2) - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 2) + (costPerHalfDay * 3) - freeChildCare} if 2 full days and 3 half days`, () => {
+    const daysArray = ['full', 'full', 'half', 'half', 'half'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 2) + (costPerHalfDay * 3) - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 3) - freeChildCare} if 3 full days and 0 half days`, () => {
+    const daysArray = ['full', 'full', 'full', 'none', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 3) - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 3) + costPerHalfDay - freeChildCare} if 3 full days and 1 half day`, () => {
+    const daysArray = ['full', 'full', 'full', 'half', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 3) + costPerHalfDay - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 3) + (costPerHalfDay * 2) - freeChildCare} if 3 full days and 2 half days`, () => {
+    const daysArray = ['full', 'full', 'full', 'half', 'half'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 3) + (costPerHalfDay * 2) - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 4) - freeChildCare} if 4 full days and 0 half days`, () => {
+    const daysArray = ['full', 'full', 'full', 'full', 'none'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 4) - freeChildCare);
+  });
+  it(`should return ${(costPerDay * 4) + costPerHalfDay - freeChildCare} if 4 full days and 1 half day`, () => {
+    const daysArray = ['full', 'full', 'full', 'full', 'half'];
+    expect(getCostPerWeek(daysArray, age, 30)).equal((costPerDay * 4) + costPerHalfDay - freeChildCare);
+  });
+});
