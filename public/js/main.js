9a416ce2f4d70bd5b6d566a7f2c953eca068cc57
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-11-19 15:11:49 +0000

    Updated template and styles

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,9 @@
 $(document).ready(function () {
   $('.js-hidden').hide();
-  showHide();
 });
 
 $(document).on('click', function () {
-  showHide();
+  $('[data-target]').each(function () {
+    
+  });
 });
-
-function showHide () {
-  if ($('#freeCare-yes').is(':checked')) {
-    $('#childcare-hidden').show();
-  } else {
-    $('#childcare-hidden').hide();
-    $('#childcare-hidden').find('input').each(function () {
-      $(this).prop('checked', false);
-    });
-  }
-
-  if ($('#fullTime-no').is(':checked')) {
-    $('#part-time-hidden').show();
-  } else {
-    $('#part-time-hidden').hide();
-    $('#part-time-hidden').find('input').each(function () {
-      $(this).prop('checked', false);
-    });
-  }
-}
