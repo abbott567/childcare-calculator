78aba12c430a83461efcdd8766e5c581c0f41701
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2018-11-20 20:19:12 +0000

    Updated cost per week function

--- functions/getCostPerWeek.js
+++ functions/getCostPerWeek.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const {getCostPerDay} = require('../functions/getCostPerDay');
-const {getCostPerHalfDay} = require('../functions/getCostPerHalfDay');
+const {getCostPerDay} = require('./getCostPerDay');
+const {getCostPerHalfDay} = require('./getCostPerHalfDay');
+const {getCostForFreeHours} = require('./getCostForFreeHours');
 
 function getCostPerWeek(daysArray, age, freeHours) {
   const costPerDay = getCostPerDay(age);
   const costPerHalfDay = getCostPerHalfDay(age);
+  const costOfFreeHours = getCostForFreeHours(freeHours, age);
   let costPerWeek = 0;
 
   for (let i = 0; i < daysArray.length; i++) {
@@ -17,18 +19,7 @@ function getCostPerWeek(daysArray, age, freeHours) {
     }
   }
 
-  if (freeHours === '15' || freeHours === 15) {
-    if (age === 2) {
-      costPerWeek -= 78;
-    }
-    if (age === 3) {
-      costPerWeek -= 65.10;
-    }
-  }
-
-  if (freeHours === '30' || freeHours === 30) {
-    costPerWeek -= 130.20;
-  }
+  costPerWeek -= costOfFreeHours;
 
   if (costPerWeek < 0) {
     costPerWeek = 0;
